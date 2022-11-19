6d6b1321a3f8cd4212e15e39cf8e544d6e21184e
Nikolay <Onedaydejavu124@mail.ru>
2022-11-20 00:05:25 +0700

    Напишите программу, которая принимает на вход число и выдает количество цифр в числе

---
+++ EX002/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число и выдает количество цифр в числе
+
+System.Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine() !);
+
+int i = 0;
+while(number > 0)
+{
+    number = number / 10;
+    i++;
+}
+Console.WriteLine($"{i}");
