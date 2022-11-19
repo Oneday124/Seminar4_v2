1a95b20f81c46f7b7039c72ca28ed568025333eb
Nikolay <Onedaydejavu124@mail.ru>
2022-11-20 00:41:10 +0700

    Напишите программу, которая принимает на вход число N и выдает произведение чисел от 1 до N

---
+++ EX003/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число N и выдает произведение чисел от 1 до N
+long Composition (int n)
+{
+    long composition = 1;
+    if(n <= 0) composition = -1;
+    for(int i = 1; i <= n; i++)
+    {
+        composition = composition * i;
+    }
+    return composition;
+}
+
+Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine() !);
+
+System.Console.WriteLine($"{Composition(n)}");
+
