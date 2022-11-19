f3fd9d48525b5a3c1e7aac1a16bc87d35b61d07e
Nikolay <Onedaydejavu124@mail.ru>
2022-11-20 01:58:57 +0700

    Написать цикл который принимает на вход 2 числаи возводит число А в степень Б

---
+++ EX005/Program.cs
@@ -0,0 +1,17 @@
+﻿// Написать цикл который принимает на вход 2 числаи возводит число А в степень Б
+int Exponentiation(int a, int b)
+{
+    int result = 1;
+    for(int i = 1; i <= b; i++)
+    {
+        result *= a;
+    }
+    return result;
+}
+
+System.Console.WriteLine("Введите чило А: ");
+int a = int.Parse(Console.ReadLine() !);
+System.Console.WriteLine("Введите чило B: ");
+int b = int.Parse(Console.ReadLine() !);
+
+Console.WriteLine($"{Exponentiation(a, b)}");
